2b63e8ca58a0694d552cbe8763ed5ff56346c9ba
Luana Petter <petter.luana@gmail.com>
2020-09-03 11:32:51 -0300

    325

--- src/main/java/com/luanapetter/course/services/UserService.java
+++ src/main/java/com/luanapetter/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.luanapetter.course.entities.User;
 import com.luanapetter.course.respositories.UserRepository;
+import com.luanapetter.course.services.exceptions.ResourceNotFoundException;
 
 
 
@@ -23,7 +24,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
